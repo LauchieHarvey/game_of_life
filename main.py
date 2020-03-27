f3f8d2a644e53f278b1e78b7fb8a4cadc383f372
LauchieHarvey <lauchieharvey@gmail.com>
2020-03-27 20:16:51 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame as pg
+#import random as rand
+
+def main():
+	pass
+
+
+def init_gui():
+	pass
+
+
+def initialise_board():
+	pass
+
+
+
+def update_board(board_array):
+	pass
+
+
+def update_gui(board_array):
+	pass
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
