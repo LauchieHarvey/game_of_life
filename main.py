82f300b92e918d97427d1c6da6454c76bbbf744a
LauchieHarvey <lauchieharvey@gmail.com>
2020-03-27 21:08:50 +1000

    basics of pygame implemented, working on the grid

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-import pygame as pg
+import pygame
 #import random as rand
 
+# CONSTANTS
+WIN_DIMENSIONS = (600, 600) # width, height
+BACKGROUND_COLOUR = (255, 255, 255)
+GRID_SIZE = 20
+CELL_WIDTH = WIN_DIMENSIONS[0] / GRID_SIZE
+CELL_HEIGHT = WIN_DIMENSIONS[1] / GRID_SIZE
+
+
+# ^CONSTANTS
+
 def main():
-	pass
+	window = init_gui()
+
+	game_running = True
+	while game_running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+				pygame.quit()
+			pygame.display.update()
+
 
 
 def init_gui():
-	pass
+	window = pygame.display.set_mode(WIN_DIMENSIONS)
+	pygame.display.set_caption("Game of Life :)")
+	window.fill(BACKGROUND_COLOUR)
+
+	for row_num in range(GRID_SIZE):
+		for column_num in range(GRID_SIZE):
+			pygame.draw.rect(window, (0, 0, 0), [column_num * CELL_WIDTH, row_num * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT])
+
+	return window
 
 
 def initialise_board():
