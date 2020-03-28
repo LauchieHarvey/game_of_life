9a56567952d6d758c37721f21033bd05d1a7a058
LauchieHarvey <lauchieharvey@gmail.com>
2020-03-28 21:55:28 +1000

    rendering optimisation and aesthetic changes

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import random
 
 # CONSTANTS
 WIN_DIMENSIONS = (600, 600) # width, height
-BACKGROUND_COLOUR = (255, 255, 255)
-GRID_SIZE = 20
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRID_SIZE = 40
 CELL_WIDTH = int(WIN_DIMENSIONS[0] / GRID_SIZE)
 CELL_HEIGHT = int(WIN_DIMENSIONS[1] / GRID_SIZE)
 INITIAL_CELL_COUNT = 10
@@ -38,7 +39,7 @@ def main():
 def init_gui():
 	window = pygame.display.set_mode(WIN_DIMENSIONS)
 	pygame.display.set_caption("Game of Life :)")
-	window.fill(BACKGROUND_COLOUR)
+	window.fill(BLACK)
 	return window
 
 
@@ -105,11 +106,9 @@ def update_gui(window, board_array):
 	for row_index, row_value in enumerate(board_array):
 		for column_index, column_value in enumerate(row_value):
 			if column_value == 1:
-				cell_colour = (0, 0, 0)
-			else:
-				cell_colour = BACKGROUND_COLOUR
+				pygame.draw.rect(window, WHITE, [column_index * CELL_WIDTH, row_index * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT])
 
-			pygame.draw.rect(window, cell_colour, [column_index * CELL_WIDTH, row_index * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT])
+			
 
 
 
